64d927011e8d2fcd4100dbb69fba814db8bcf855
mnebot <miguelnbt@icloud.com>
2021-11-18 12:08:33 -0500

    styled navbar with hover effect on button

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
-import logo from '../../asset/images/logos/woodshoplogo.png'
-import './Nav.css'
+import logo from "../../asset/images/logos/woodshoplogo.png";
+import "./Nav.css";
 
 const Nav = () => {
   return (
@@ -7,15 +7,17 @@ const Nav = () => {
       <div>
         <img src={logo} alt="The Wood Shop Barber Shop's Logo" />
       </div>
-      <ul>
-        <li>Our Roots</li>
-        <li>What We Do</li>
-        <li>Where We Are</li>
-        <li>The Crew</li>
-        <li>The Proof</li>
-      </ul>
-      <div>
-        <button>Book Your Cut</button>
+      <div className='ul-button-container'>
+        <ul>
+          <li>Our Roots</li>
+          <li>What We Do</li>
+          <li>Where We Are</li>
+          <li>The Crew</li>
+          <li>The Proof</li>
+        </ul>
+        <div>
+          <button>BOOK YOUR CUT</button>
+        </div>
       </div>
     </nav>
   );
